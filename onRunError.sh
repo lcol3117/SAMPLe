408077b32d7f9fc8048d0145c47119f5749d24e6
Landon Colaresi <landoncol3117@gmail.com>
2020-05-30 17:43:50 +0000

    onrunerror.sh

---
+++ onRunError.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "ERROR in Python3 run!"
+echo "Help? : does SAMPLe_algorithm/Python3/unit/main.py exist?"
+echo "Help? : check SAMPLe_algorithm/Python3/.data.toml"
--- runSAMPLe.sh
+++ runSAMPLe.sh
@@ -4,4 +4,4 @@ echo "[bash runner] Now running SAMPLe..."
 echo "[bash runner] using SAMPLe_algorithm/Python3/unit/main.py"
 echo "[bash runner] check at SAMPLe_algorithm/Python3/.data.toml"
 echo "[bash runner] Now running..."
-python3 /SAMPLe_algorithm/Python3/unit/main.py || bash onRunError.sh
+python3 SAMPLe_algorithm/Python3/unit/main.py || bash onRunError.sh
